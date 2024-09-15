2c1f09ab5294da0a9b6ffeacb3e96979319054a7
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2024-09-15 22:11:07 +0900

    240915 make user interface

---
+++ polymorphism_practice/src/main/java/User.java
@@ -0,0 +1,18 @@
+public interface User {
+
+    public static final boolean isAppOn = false;
+
+    public abstract void startApp();
+
+    public abstract void chooseFirstSong();
+
+    public abstract void playFirstSong();
+
+    public abstract void chooseSecondSong();
+
+    public abstract void playSecondSong();
+
+    public abstract void stopSong();
+
+    public abstract void finishApp();
+}
